25272aa0810157df029ce8e523c4c4ae6ea546b5
Ryan Galea <Ryan.Galea.c10264@mcast.edu.mt>
2020-10-26 10:28:39 +0100

    Sir code

---
+++ Solution3/PresentationWebApp/Controllers/SupportController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationWebApp.Controllers
+{
+    public class SupportController : Controller
+    {
+        [HttpGet]
+        public IActionResult Contact() //this will be used to load the page
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(string email, string query) //this will be used to handle the form submission
+        {
+
+            //...inform the responsible staff
+
+            ViewData["feedback"] = "Thank you for getting in touch with us. We will answer back asap";
+
+            return View();
+        }
+    }
+}
